e5be00ca984b0c3f8f2057428346d1487b073fc0
olgamaluhacodacy <olga.maluha@codacy.com>
2022-12-29 16:37:55 +0100

    some minor changes 4

--- src/main/java/com/codacy/utils/Math.java
+++ src/main/java/com/codacy/utils/Math.java
@@ -41,7 +41,7 @@ public class Math {
             System.out.println("hey");
             System.out.println("hey");
             System.out.println("hey");
-            return x + y;
+            return x + y + some;
         }
     }
 }
